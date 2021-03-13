3fc2a4df09aab5f35444e7dd1049954792c7da73
henri_de_bel <henri.de-bel@capgemini.com>
2021-03-13 15:33:05 +0100

    first commit

---
+++ app/src/main/java/com/example/sharingapp/Contact.java
@@ -0,0 +1,58 @@
+package com.example.sharingapp;
+
+import java.util.UUID;
+
+public class Contact extends Observable{
+
+    private String id;
+    private String username;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+        notifyObservers();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+        notifyObservers();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+
+
+    public Contact(String username, String email, String id) {
+        this.username = username;
+        this.email = email;
+
+        if (id == null){
+            setId();
+        } else {
+            updateId(id);
+        }
+    }
+
+    public void setId() {
+        this.id = UUID.randomUUID().toString();
+        notifyObservers();
+    }
+
+    public void updateId(String id){
+        this.id = id;
+        notifyObservers();
+    }
+
+
+}
